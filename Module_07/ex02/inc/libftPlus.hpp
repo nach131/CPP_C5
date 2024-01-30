85ccdf99979cae59ea467ccbc7f3b7ac34fce568
Nacho Mota 🚀 <nacho.mota@gmail.com>
2024-01-30 10:47:59 +0100

    ex02 init

---
+++ Module_07/ex02/inc/libftPlus.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftPlus.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/25 10:14:26 by nmota-bu          #+#    #+#             */
+/*   Updated: 2024/01/29 21:41:58 by nmota-bu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_P_HPP
+#define LIBFT_P_HPP
+
+#include <iostream>
+#include <string>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+void printStringInCenter(const std::string &str);
+
+#endif
