2a9979d32f1931a84141f5d75be7c436849fe7c6
Nacho Mota 🚀 <nacho.mota@gmail.com>
2024-02-01 20:46:30 +0100

    m8 ex00 ok

--- Module_08/ex00/inc/easyfind.hpp
+++ Module_08/ex00/inc/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 19:37:07 by nmota-bu          #+#    #+#             */
-/*   Updated: 2024/01/31 22:03:06 by nmota-bu         ###   ########.fr       */
+/*   Updated: 2024/02/01 20:44:43 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,43 +45,5 @@ void easyfindMap(const T &container, int n)
 	else
 		throw std::string("Value '" + std::to_string(n) + "' doesn't exist in the container.");
 }
-
-// template <typename T>
-// bool isMap(const T &container)
-// {
-// 	if (typeid(container) == typeid(std::map<int, int>))
-// 		return true;
-// 	return false;
-// }
-
-// =================================================================
-
-// template <typename T>
-// void easyfind(const T &container, int n)
-// {
-// 	(void)n;
-// 	if (isMap(container))
-// 	{
-// 		std::cout << "Siii" << std::endl;
-// 		typename T::const_iterator it = container.find(n);
-
-// 		if (it != container.end())
-// 			std::cout << GREEN << "Value '" + std::to_string(n) + "' exists." << RESET << std::endl;
-// 		else
-// 			throw std::string("Value '" + std::to_string(n) + "' doesn't exist in the container.");
-// 	}
-// 	else
-// 	{
-// 		std::cout << "noo" << std::endl;
-
-// 		// typename T::const_iterator it = std::find(container.begin(), container.end(), n);
-
-// 		// if (it != container.end())
-// 		// if (std::find(container.begin(), container.end(), n))
-// 		// 	std::cout << GREEN << "Value '" + std::to_string(n) + "' exists." << RESET << std::endl;
-// 		// else
-// 		// 	throw std::string("Value '" + std::to_string(n) + "' doesn't exist in the container.");
-// 	}
-// }
 
 #endif
